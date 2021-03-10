5ca38dfa041a71ba3d318d2b968bec7ab08ef249
Denisbaic <denisbaic@yandex.ru>
2021-03-11 02:30:51 +0300

    add plugin to project

---
+++ Plugins/FLDPlugin/Source/FLDPlugin/Private/FLDPluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FLDPluginBPLibrary.h"
+#include "FLDPlugin.h"
+
+UFLDPluginBPLibrary::UFLDPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UFLDPluginBPLibrary::FLDPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
